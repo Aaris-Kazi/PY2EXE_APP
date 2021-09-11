d3b1f655a6804d15802d724108dcc1a71a99d04f
Aaris-Kazi <aariskazi@gmail.com>
2021-09-11 12:48:59 +0530

    some of more changes

--- main.py
+++ main.py
@@ -7,18 +7,22 @@ output = ''
 def main():
     app = Tk()
     
-    text = Text(app)
-    text.pack()
+    # text = Text(app)
+    # text.pack()
     
-    def test():
-        # os.system('python test.py')
-        p = sub.Popen('pip install pyinstaller',stdout=sub.PIPE,stderr=sub.PIPE)
-        output, errors = p.communicate()
-        text.insert(END, output)
+    # def test():
+    #     # os.system('python test.py')
+    #     p = sub.Popen('pip install pyinstaller',stdout=sub.PIPE,stderr=sub.PIPE)
+    #     output, errors = p.communicate()
+    #     text.insert(END, output)
     
 
-    b1 = Button(app, text= 'Transform', command = lambda: test())
-    b1.pack()
+    # b1 = Button(app, text= 'INSTALL MODULE', command = lambda: test())
+    # b1.pack()
+
+    frame = Frame(app, width= 200, height = 480, bg = 'blue')
+    frame.place(x = 0, y = 0)
+    # frame.pack(x = 0, y = 0)
 
     
     app.title('Transformer')
