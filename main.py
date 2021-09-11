850c6212691ccdb4c96b475e749e303df311be33
Aaris-Kazi <aariskazi@gmail.com>
2021-09-11 10:39:22 +0530

    We are able to run things on command prompt

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from tkinter import *
+import os
+
+def test():
+    os.system('python test.py')
+    # print('Hello world')
 
 def main():
     app = Tk()
+
+    b1 = Button(app, text= 'Transform', command = lambda: test())
+    b1.pack()
+
+
     app.title('Transformer')
     app.geometry('720x480')
     app.mainloop()
