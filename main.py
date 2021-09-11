3e2dbba8cfeaed3e9fed41a65186e111fa631159
Aaris-Kazi <aariskazi@gmail.com>
2021-09-11 10:49:58 +0530

    now we can see command prompt on the screen

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from tkinter import *
 import os
+import subprocess as sub
 
-def test():
-    os.system('python test.py')
-    # print('Hello world')
+output = ''
+
+
+
 
 def main():
     app = Tk()
+    
+    text = Text(app)
+    text.pack()
+    
+    def test():
+        # os.system('python test.py')
+        p = sub.Popen('python test.py',stdout=sub.PIPE,stderr=sub.PIPE)
+        output, errors = p.communicate()
+        text.insert(END, output)
+    
 
     b1 = Button(app, text= 'Transform', command = lambda: test())
     b1.pack()
 
-
+    
     app.title('Transformer')
     app.geometry('720x480')
     app.mainloop()
