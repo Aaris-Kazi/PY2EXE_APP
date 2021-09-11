e6a3b71d018e9be343dc2e074d5377a38122f626
Aaris-Kazi <aariskazi@gmail.com>
2021-09-11 17:06:21 +0530

    modules testing and running

--- main.py
+++ main.py
@@ -12,29 +12,27 @@ def main():
         print(filename)
     
     text1 = Text(app)
-    b1 = Button(app, text= 'INSTALL MODULE')
+    b1 = Button(app, text= 'INSTALL MODULE', command = lambda: test())
     words = Entry(app)
     open_button = Button(app, text = 'OPEN FILE', command = lambda: openfile())
+    def test():
+        # os.system('python test.py')
+        p = sub.Popen('pip install pyinstaller',stdout=sub.PIPE,stderr=sub.PIPE)
+        output, errors = p.communicate()
+        text1.insert(END, output)
 
     def installer():
         words.place_forget()
         open_button.place_forget()
         text1.place(x = 220, y = 10)
-        # b1 = Button(app, text= 'INSTALL MODULE', command = lambda: test())
         b1.place(x = 460, y = 400)
+
     def converter():
         open_button.place(x = 300, y = 10)
         words.place(x = 220, y = 10)
         text1.place_forget()
         b1.place_forget()
-    # def test():
-    #     # os.system('python test.py')
-    #     p = sub.Popen('pip install pyinstaller',stdout=sub.PIPE,stderr=sub.PIPE)
-    #     output, errors = p.communicate()
-    #     text.insert(END, output)
     
-
-
     frame = Frame(app, width= 200, height = 480, bg = 'blue')
     install_module = Button(app, text = 'INSTALL MODULES', command = lambda: installer())
     convert = Button(app, text = 'CONVERT TO EXE   ', command = lambda: converter())
