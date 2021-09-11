3d50035e99e7d961dfb7ab39866567634cecd2d9
Aaris-Kazi <aariskazi@gmail.com>
2021-09-11 15:03:09 +0530

    UI Changes

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ from tkinter import *
 import os
 import subprocess as sub
 from threading import Thread
-
+from tkinter import filedialog
 output = ''
 
 def main():
     app = Tk()
+    def openfile():
+        filename=filedialog.askopenfile(initialdir='GUI/',title="Select a file",filetypes=(("txt files","*.txt"),("All files",'*')))
+        print(filename)
+    
     text1 = Text(app)
     b1 = Button(app, text= 'INSTALL MODULE')
+    words = Entry(app)
+    open_button = Button(app, text = 'OPEN FILE', command = lambda: openfile())
+
     def installer():
+        words.place_forget()
+        open_button.place_forget()
         text1.place(x = 220, y = 10)
         # b1 = Button(app, text= 'INSTALL MODULE', command = lambda: test())
         b1.place(x = 460, y = 400)
     def converter():
+        open_button.place(x = 300, y = 10)
+        words.place(x = 220, y = 10)
         text1.place_forget()
         b1.place_forget()
     # def test():
